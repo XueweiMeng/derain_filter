59d491bf13e62f8cdf373743d56d24c30ed3a55c
XiaLiPKU <ethanlee@pku.edu.cn>
2019-06-04 10:43:37 +0800

    Update training scripts

--- scripts/build_dt.sh
+++ scripts/build_dt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH_TO_DERAIN_DATASET='/path/to/dataset'
 cd ..
-
-python build_dt.py --images_folder=datasets/Rain100H --dataset_folder=datasets/test_Rain100H --mode=full
+python build_dt.py --images_folder=datasets/Rain100H/train --dataset_folder=datasets/train_Rain100H --mode=patch --crop_size=64 --stride=32 #for train 
+#python build_dt.py --images_folder=datasets/Rain100H --dataset_folder=datasets/test_Rain100H --mode=full  # for test
 
 cd ./scripts
