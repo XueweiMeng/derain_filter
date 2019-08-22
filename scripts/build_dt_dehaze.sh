27cd2afb30fca9622d2458a2f75f316e143fa795
XiaLiPKU <ethanlee@pku.edu.cn>
2019-08-22 15:44:46 +0800

    Update dehaze scripts

--- scripts/build_dt_dehaze.sh
+++ scripts/build_dt_dehaze.sh
@@ -2,7 +2,7 @@
 PATH_TO_DERAIN_DATASET='/path/to/dataset'
 cd ..
 #dehaze
-python build_dt_dehaze.py --images_folder=defog_dataset/train_datasets --dataset_folder=defog_dataset/train_defog --mode=patch --crop_size=64 --stride=64 #for train
-python build_dt_dehaze.py --images_folder=defog_dataset/test_datasets --dataset_folder=defog_dataset/test_defog --mode=full  # for test
+python build_dt.py --images_folder=defog_dataset/train_datasets --dataset_folder=defog_dataset/train_defog --mode=patch --crop_size=64 --stride=64 #for train
+python build_dt.py --images_folder=defog_dataset/test_datasets --dataset_folder=defog_dataset/test_defog --mode=full  # for test
 
 cd ./scripts
