bac766ad09112c370971fc187e23a3390b821148
XiaLiPKU <ethanlee@pku.edu.cn>
2019-08-22 15:33:37 +0800

    Update dehaze training scripts

--- scripts/export_model_dehaze.sh
+++ scripts/export_model_dehaze.sh
@@ -6,6 +6,6 @@ NET_DEPTH=7
 
 cd ..
 
-python3 export.py --output_dir=$OUTPUT_DIR --ckpt_dir=$CKPT_DIR --channel_num=$CHANNEL_NUM --net_depth=$NET_DEPTH
+python3 export_dehaze.py --output_dir=$OUTPUT_DIR --ckpt_dir=$CKPT_DIR --channel_num=$CHANNEL_NUM --net_depth=$NET_DEPTH
 
 cd ./scripts
--- scripts/train_eval_dehaze.sh
+++ scripts/train_eval_dehaze.sh
@@ -19,7 +19,7 @@ STEPS_PER_LOG=100
 
 cd ../
 
-python3 train.py --batch_size=$BATCH_SIZE --channel_num=$CHANNEL_NUM --net_depth=$NET_DEPTH --dataset_path=$TRAIN_DATASET_PATH --buffer_size=$SHUFFLE_BUFFER_SIZE --learning_rate=$LEARNING_RATE --num_steps=$TRAIN_STEPS --save_num=$TRAIN_SAVE_NUM --steps_per_log=$STEPS_PER_LOG --steps_per_save=$STEPS_PER_SAVE --logdir=$TRAIN_LOGDIR
-python3 eval.py --batch_size=1 --channel_num=$CHANNEL_NUM --net_depth=$NET_DEPTH --dataset_path=$TEST_DATASET_PATH --ckpt_dir=$TRAIN_LOGDIR --num_steps=$TEST_STEPS --save_num=$TEST_SAVE_NUM --logdir=$TEST_LOGDIR
+python3 train_dehaze.py --batch_size=$BATCH_SIZE --channel_num=$CHANNEL_NUM --net_depth=$NET_DEPTH --dataset_path=$TRAIN_DATASET_PATH --buffer_size=$SHUFFLE_BUFFER_SIZE --learning_rate=$LEARNING_RATE --num_steps=$TRAIN_STEPS --save_num=$TRAIN_SAVE_NUM --steps_per_log=$STEPS_PER_LOG --steps_per_save=$STEPS_PER_SAVE --logdir=$TRAIN_LOGDIR
+python3 eval_dehaze.py --batch_size=1 --channel_num=$CHANNEL_NUM --net_depth=$NET_DEPTH --dataset_path=$TEST_DATASET_PATH --ckpt_dir=$TRAIN_LOGDIR --num_steps=$TEST_STEPS --save_num=$TEST_SAVE_NUM --logdir=$TEST_LOGDIR
 
 cd ./scripts
